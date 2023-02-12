20ba46b2f28bb2a76b9999f6fb496ddda5cc307d
Lyoooooo <81254893+Lyoooooo@users.noreply.github.com>
2023-02-12 21:06:50 +0100

    maquette index

---
+++ fonction.php
@@ -0,0 +1,42 @@
+<?php
+function footer()
+{
+?>
+    <footer class='text-center text-white' style='background-color: rgba(0, 0, 0, 0.904);color:white; width: 100%;'>
+    
+        <div class='container pt-4'>
+            <section class='text-center text-light'>
+                <p>
+                    NOMDUSITE est un site deposé par JESAISPAS. <br>
+                    Tous droits réservés.
+                </p>
+            </section>
+        </div>
+        <div class='text-center text-light p-3' style='background-color: rgba(0, 0, 0, 0.2);'>
+            © 2022 NOMDUSITE<br>
+        </div>
+    </footer>
+<?php
+}
+
+function mainHeader()
+{
+?>
+    <nav class="navbar navbar-expand-lg bg-dark" data-bs-theme="dark">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="#" >NOMDUSITE</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                <div class="navbar-nav">
+                    <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+                    <a class="nav-link active" href="seances.php">Séances</a>
+                    <a class="nav-link active" href="gazette.php">Gazette</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+<?php
+}
+?>
---
+++ index.php
@@ -0,0 +1,66 @@
+<?php
+include "fonction.php";
+mainHeader();
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="style.css">
+    <script src="back.js"></script>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/js/bootstrap.bundle.min.js" integrity="sha384-w76AqPfDkMBDXo30jS1Sgez6pr3x5MlQ1ZAGC+nuZB+EYdgRZgiwxhTBTkF7CXvN" crossorigin="anonymous"></script>
+</head>
+
+<body>
+
+    <div id="placeholderCarrou" style="width: 80%; height: 400px; background-color:grey; margin-top: 20px; margin-bottom: 20px; margin-left: 10%;">
+        EMPLACEMENT POUR LES BANNIERES
+    </div>
+
+    <div class="hr" style="width: 90%; margin-top: 20px; margin-bottom: 20px; margin-left: 5%;">
+        <hr>
+    </div>
+
+    <div id="bigInsta" style="width: 80%; height: 500px; background-color:darkred; margin-top: 20px; margin-bottom: 20px; margin-left: 10%;">
+        <div class="container text-center">
+            <div class="row">
+                <div class="col">
+                    <div class="post" style="width: 80%; height:400px; background-color:grey; margin-top: 50px; margin-bottom: 50px; margin-left: 10%; position:relative">
+                        POST INSTA
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="post" style="width: 80%; height:400px; background-color:grey; margin-top: 50px; margin-bottom: 50px; margin-left: 10%; position:relative">
+                        POST INSTA
+                    </div>
+                </div>
+                <div class="col">
+                    <div class="post" style="width: 80%; height:400px; background-color:grey; margin-top: 50px; margin-bottom: 50px; margin-left: 10%; position:relative">
+                    POST INSTA
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="hr" style="width: 90%; margin-top: 20px; margin-bottom: 20px; margin-left: 5%;">
+        <hr>
+    </div>
+
+
+    <div id="placeholderCarrou" style="width: 60%; height: 400px; background-color:bisque; margin-top: 20px; margin-bottom: 20px; margin-left: 20%;">
+        EMPLACEMENT POUR NEWSLETTER
+    </div>
+
+</body>
+<?php
+footer();
+?>
+
+</html>
