4e021d1b0a4c4930dff33112fc0fdeba6b3e46d3
Lyoooooo <81254893+Lyoooooo@users.noreply.github.com>
2023-02-22 16:51:21 +0100

    gazette et créa articleIndiv

--- gazette.php
+++ gazette.php
@@ -22,7 +22,80 @@ mainHeader();
     <div class="titre">
         GAZETTE
     </div>
+    <div class="boxArticles">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Filtrer
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Ordre
+                        </a>
+                        <ul class="dropdown-menu">
+                            <li><a class="dropdown-item" href="#">Action</a></li>
+                            <li><a class="dropdown-item" href="#">Another action</a></li>
+                            <li>
+                                <hr class="dropdown-divider">
+                            </li>
+                            <li><a class="dropdown-item" href="#">Something else here</a></li>
+                        </ul>
+                    </li>
+                </ul>
+                <form class="d-flex" role="search">
+                    <input class="form-control me-2" type="search" placeholder="Article" aria-label="Search">
+                    <button class="btn btn-outline-success" type="submit">Chercher</button>
+                </form>
+            </div>
+        </div>
+    </nav>
+    </div>
 
+
+    <?php
+
+    $nbr = 0;
+    while ($nbr < 3) { ?> <!-- WHILE il rest des articles (ordre à voir si on fait un tri) -->
+
+        <div class="boxArticles">
+            <hr>
+            <div class="row" style="height: 150px;">
+                <div class="col-2">
+                    <div class="photoSeance" style="margin-left:20%">
+                        <img src="images/placeholderAffiche.jpg" alt="">
+                    </div>
+                </div>
+                <div class="col-10">
+                    <div class="titreArticle">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Eveniet ratione repellendus consectetur dolorum itaque, ipsam quia debitis ea saepe, vel quibusdam autem quas earum vitae laborum odio quo! Quo, fugiat.
+                    </div>
+                    <div class="descriArticle">
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda, perspiciatis laborum sapiente unde velit veritatis fuga? Molestiae maiores nesciunt ad. Corporis dicta accusamus culpa est unde, quo dolorem dignissimos perferendis?
+                    </div>
+                    <div>
+                        <a href="articleIndiv.php">Lire la suite></a>
+                    </div>
+                </div>
+            </div>
+            <hr>
+        </div>
+
+
+    <?php $nbr++;
+    }
+    ?>
 </body>
 <?php
 footer();
