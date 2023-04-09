49f30b2bf32272268e6759544fe5962161182350
Lyoooooo <81254893+Lyoooooo@users.noreply.github.com>
2023-04-09 18:12:03 +0200

    carrousel index

--- index.php
+++ index.php
@@ -24,11 +24,41 @@ $tab = $res->fetchAll();
 </head>
 
 <body>
+
+    <div id="placeholderCarrou">
+        <div id="carouselExample" class="carousel slide" style="width: 100%; aspect-ratio: 8 / 1; overflow: hidden;">
+            <!-- METTRE UNE RECUPERATION DANS LA BDD -->
+
+                <div class="carousel-indicators">
+                    <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                    <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                    <button type="button" data-bs-target="#carouselExample" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                </div>
+                <div class="carousel-inner">
+                    <div class="carousel-item active">
+                        <img src="images/banner1.webp" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/banner2.jpg" class="d-block w-100" alt="...">
+                    </div>
+                    <div class="carousel-item">
+                        <img src="images/banner3.webp" class="d-block w-100" alt="...">
+                    </div>
+                </div>
+                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Previous</span>
+                </button>
+                <button class="carousel-control-next" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="visually-hidden">Next</span>
+                </button>
+
+        </div>
+    </div>
+
     <div class="titre">
         CONTRE PLONGÉE CINÉ
-    </div>
-    <div id="placeholderCarrou" style="width: 80%; height: 400px; background-color:grey; margin-top: 20px; margin-bottom: 20px; margin-left: 10%;">
-        EMPLACEMENT POUR LES BANNIERES
     </div>
 
     <div class="hr" style="width: 90%; margin-top: 20px; margin-bottom: 20px; margin-left: 5%;">
@@ -106,10 +136,6 @@ $tab = $res->fetchAll();
             </div>
         </div>
     </div>
-
-
-
-
 </body>
 <?php
 footer();
